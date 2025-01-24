7990ed3f16d6886b1b4ad9531dce70b128d092f1
Hwijae kim <113835565+HwijaeKim@users.noreply.github.com>
2025-01-25 01:46:12 +0900

    update react01

--- react01/src/App.jsx
+++ react01/src/App.jsx
@@ -6,29 +6,20 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  let post = '변수선언';
+
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+
+      <div className="black-nav">
+        <h4>Code Blog.</h4>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <h4>{ post }</h4>
+
+      <h4 style={ {color: 'blue', fontSize: '24px'} }>JSX에서 Inline Style은 다음과 같이 작성해야 합니다.</h4>
+
+    </div>
   )
 }
 
