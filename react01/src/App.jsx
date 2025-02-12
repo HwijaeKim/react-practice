89bf5651792da313769b53994ee8d1eb30898d1b
Hwijae kim <113835565+HwijaeKim@users.noreply.github.com>
2025-02-12 22:34:42 +0900

    state

--- react01/src/App.jsx
+++ react01/src/App.jsx
@@ -6,18 +6,55 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  let post = '변수선언';
+  let test = '변수선언';
+
+  // a = 실제 변수값, b = state 변경을 도와주는 함수
+  // 굳이 useState?: 그냥 변수는 변수값이 바뀌면 HTML에 자동으로 반영되지 않지만 useState는 변수값이 바뀌면 실시간으로 HTML에 반영됨
+  let [post, changePost] = useState(['HTML 기초', 'CSS 기초', 'JavaScript 기초']);
+  let [like, changeLike] = useState(0);
+
+  function likeF() {
+    changeLike(like++)
+  }
+
+  // Destructuring 문법: JS의 배열과 비슷하지만 더 간편하게 작성 가능
+  // let [c, d] = [1, 2]
 
   return (
-    <div>
+    <div className='App'>
+
+
 
       <div className="black-nav">
         <h4>Code Blog.</h4>
       </div>
 
-      <h4>{ post }</h4>
+      {/* ... == 괄호 벗겨주세요(새로운 화살표로 저장해 주세요~) */}
+      <button onClick={
+        () => {
+          let copy = [...post];
+          copy[0] = '여자코트추천'
+          changePost(copy)
+        }
 
-      <h4 style={ {color: 'blue', fontSize: '24px'} }>JSX에서 Inline Style은 다음과 같이 작성해야 합니다.</h4>
+      }>글 수정</button>
+
+    <div className='list'>
+      <h4>{ post[0] } <span onClick={likeF}>👍</span> {like} </h4>
+      <p>2월 12일 발행</p>
+    </div>
+
+    <div className='list'>
+      <h4>{ post[1] }</h4>
+      <p>2월 12일 발행</p>
+    </div>
+
+    <div className='list'>
+      <h4>{ post[2] }</h4>
+      <p>2월 12일 발행</p>
+    </div>
+
+
 
     </div>
   )
