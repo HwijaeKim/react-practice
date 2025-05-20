1ad6914bb082416d7b49dd120567becfdef4ad02
Hwijae kim <113835565+HwijaeKim@users.noreply.github.com>
2025-05-21 08:56:48 +0900

    react01[1-9] / map: 많은 div들을 반복문으로 줄이고 싶은 충동이 들 때
    숙제 전

--- react01_codingapple/src/App.jsx
+++ react01_codingapple/src/App.jsx
@@ -9,13 +9,14 @@ function App() {
   // a = 실제 변수값, b = state 변경을 도와주는 함수
   // 굳이 useState?: 그냥 변수는 변수값이 바뀌면 HTML에 자동으로 반영되지 않지만 useState는 변수값이 바뀌면 실시간으로 HTML에 반영됨
   let [post, changePost] = useState(['여자코트추천', '강남우동맛집', '파이썬독학']);
-  let [like, changeLike] = useState(0);
+  let [like, changeLike] = useState([0]);
 
   let [modal, setModal] = useState(false);
 
   function likeF() {
     changeLike(like++)
   }
+
 
   // Destructuring 문법: JS의 배열과 비슷하지만 더 간편하게 작성 가능
   // let [c, d] = [1, 2]
@@ -39,7 +40,7 @@ function App() {
 
       }>가나다순 정렬</button>
 
-      <div className='list'>
+      {/* <div className='list'>
         <h4>{post[0]} <span onClick={likeF}>👍</span> {like} </h4>
         <p>2월 12일 발행</p>
       </div>
@@ -52,12 +53,24 @@ function App() {
       <div className='list'>
         <h4 onClick={() => { setModal(modal == true ? false : true) }}>{post[2]}</h4>
         <p>2월 12일 발행</p>
-      </div>
+      </div> */}
 
-      {/* 조건식 ? 참일 떄 실행할 코드 : 거짓일 때 실행할 코드 */}
-      {
-        modal == true ? <Modal /> : null
-      }
+    {
+      post.map(function(a, i){
+        return (
+          <div className='list' key="i">
+            <h4>{post[i]} <span onClick={likeF}>👍</span> {like} </h4>
+            <p>2월 12일 발행</p>
+          </div>
+        )
+      })
+    }
+
+
+    {/* 조건식 ? 참일 떄 실행할 코드 : 거짓일 때 실행할 코드 */}
+    {
+      modal == true ? <Modal /> : null
+    }
 
 
 
